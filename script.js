84830c25ea7a6002d16634f366f131c502eaac2d
i-am-riya <mriya99s@yahoo.com>
2024-10-27 09:38:53 +0530

    some changes in js

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ for(let i = 1; i <= 5; i++){
 function getComputerChoice() {
     let arr = ["rock", "paper", "scissors"];
     let computerChoice = arr[Math.floor(Math.random() * arr.length)];
+    document.write(`Computer Choice: ${computerChoice} </br>`);
     return computerChoice.toUpperCase();
 };
 
 function getHumanChoice() {
     let humanChoice = prompt("Chose one - rock,paper, or scissors");
+    document.write(`Your Choice: ${humanChoice} </br>`);
     return humanChoice.toUpperCase();
 }
 
@@ -23,13 +25,17 @@ function playRound(humanChoice, computerChoice) {
 
     if (humanChoice === computerChoice) {
         console.log("This round is tie");
+        document.write("This round is tie" + "</br>");
+
     } else if (humanChoice === "ROCK" && computerChoice === "SCISSORS" || humanChoice === "SCISSORS" && computerChoice === "PAPER" || humanChoice === "PAPER" && computerChoice === "ROCK"
     ) {
         console.log("You win!");
+        document.write("You Win!" + "</br>");
         humanScore++;
         // console.log(humanScore);
     } else {
         console.log("You Lose!");
+        document.write("You Lose!" + "</br>");
         computerScore++;
         // console.log(computerScore);
     }
@@ -44,12 +50,13 @@ playRound(humanSelection, computerSelection);
 console.log(humanScore);
 console.log(computerScore);
 
+document.write(`Your Score: ${humanScore}`);
+document.write(`Computer Score: ${computerScore} </br> </br>`);
+
 }
 }
 
 playGame(humanScore, computerScore)
-document.write(`Your Score: ${humanScore}`);
-document.write(`Computer Score: ${computerScore}`);
 
 
 
